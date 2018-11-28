0bb2ad54d92aae73531297514741c4dfa368b5b3
Bob <bob@clough.me>
2018-11-28 13:39:49 +0000

    Initial Version

---
+++ mbddns/__init__.py
@@ -0,0 +1,35 @@
+import async
+import asyncio
+import aiohttp
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+async def update(domain, password, host, ttl=60):
+    data = {
+        'domain': domain,
+        'password': password,
+        'command': "REPLACE {} {} A DYNAMIC_IP".format(host, ttl)
+    }
+
+    async with aiohttp.ClientSession() as session:
+        try:
+            resp = await asyncio.gather(session.post('https://dnsapi4.mythic-beasts.com/',data=data))
+            body = await resp[0].text()
+
+            if body.startswith("REPLACE"):
+                _LOGGER.debug("Updating Mythic Beasts successful: %s", body)
+                return True
+
+            if body.startswith("ERR"):
+                _LOGGER.error("Updating Mythic Beasts failed: %s",
+                              body.partition(' ')[2])
+
+            if body.startswith("NREPLACE"):
+                _LOGGER.warning("Updating Mythic Beasts failed: %s",
+                                body.partition(';')[2])
+
+        except Exception as e:
+            _LOGGER.error("Updating Mythic Beasts failed: %s", e)
+
+        return False
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='mbddns',
+      version='0.1',
+      description='Mythic Beasts Dynamic DNS updater',
+      url='https://github.com/thinkl33t/mb-ddns',
+      author='Bob Clough',
+      author_email='bob@clough.me',
+      license='MIT',
+      packages=['mbddns'],
+      install_requires=[
+          'aiohttp',
+      ],
+      zip_safe=False)
