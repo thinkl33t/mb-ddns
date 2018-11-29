c8e8631a34afa50ff90efcf0f0f5c194a6fdee90
Bob <bob@clough.me>
2018-11-29 16:08:54 +0000

    Allow using existing session

--- mbddns/__init__.py
+++ mbddns/__init__.py
@@ -4,14 +4,14 @@ import logging
 
 _LOGGER = logging.getLogger(__name__)
 
-async def update(domain, password, host, ttl=60):
+async def update(domain, password, host, ttl=60, session=None):
     data = {
         'domain': domain,
         'password': password,
         'command': "REPLACE {} {} A DYNAMIC_IP".format(host, ttl)
     }
 
-    async with aiohttp.ClientSession() as session:
+    async with session or aiohttp.ClientSession() as session:
         try:
             resp = await asyncio.gather(session.post('https://dnsapi4.mythic-beasts.com/',data=data))
             body = await resp[0].text()
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mbddns',
-      version='0.1.1',
+      version='0.1.2',
       description='Mythic Beasts Dynamic DNS updater',
       url='https://github.com/thinkl33t/mb-ddns',
       author='Bob Clough',
